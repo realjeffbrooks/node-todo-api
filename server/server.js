0d22660b39481b6c8dcd7289a800c87fd97826a8
triomedia <31011004+triomedia@users.noreply.github.com>
2017-09-18 18:54:10 -0600

    GET /todos testing

--- server/server.js
+++ server/server.js
@@ -12,13 +12,13 @@ app.use(bodyParser.json());
 app.post('/todos', (req, res) => {
   var todo = new Todo({
     text: req.body.text
-  })
+  });
 
   todo.save().then((doc) => {
     res.send(doc);
   },(e) => {
     res.status(400).send(e);
-  })
+  });
 });
 
 app.get('/todos', (req, res) => {
@@ -26,7 +26,7 @@ app.get('/todos', (req, res) => {
     res.send({todos})
   }, (e) => {
     res.status(400).send(e);
-  })
+  });
 });
 
 
