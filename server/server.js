ae8dedf867106dbd194a26bee66306c124c90f2a
triomedia <31011004+triomedia@users.noreply.github.com>
2017-09-24 10:28:01 -0600

    POST /users/login

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const _ = require('lodash');
 const {ObjectID} = require('mongodb');
 var express = require('express');
 var bodyParser = require('body-parser');
+const bcrypt = require('bcryptjs');
 
 var mongoose = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -111,6 +112,24 @@ app.post('/users', (req, res) => {
 app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
 });
+
+// POST /users/login {email, password}
+// pick off email and pwd
+// res.send(body)
+app.post('/users/login', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredentials(body.email, body.password).then((user) => {
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    });
+
+  }).catch((e) => {
+    res.status(400).send();
+  });
+  
+});
+
 
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
